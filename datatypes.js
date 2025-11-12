e1163f4b00879ff676fec1c505b2e6da4e214080
Sushant Khanal <skkhanal45@gmail.com>
2025-11-12 20:53:12 +0545

    datatype cleared

---
+++ datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+//alert ("hello") // we are using node js not btowser..not in node as its not document and as js engine is in browser.
+
+let name = "suhant"// string
+let age = 18 //number
+let isLoggedIn = true //boolean
+
+// number  => 2 to power 53
+// bigint => bigintegers
+// string => ""
+// boolean => true / false
+// null => standalone value [ let state = null] representation of empty value..if the system sends temp = 0 degC due to fault in system then its preferred as null because 0 is also read as tempareture..that means the machine cant read the temperature..
+
+// undefined => not defined with value yet; only variable declared [let state;]
+//symbol => unique
+
+//object
+
+console.log(typeof "hello");//string
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
+
+
