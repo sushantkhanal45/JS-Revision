2b41dfb799f3763b03bddafe7db1d7fe22b19fa4
Sushant Khanal <skkhanal45@gmail.com>
2025-11-13 16:22:26 +0545

    comparison cleared

---
+++ comparison.js
@@ -0,0 +1,38 @@
+//avoid for practical task...
+//just for concepts; cannot be applied practically....
+
+// console.log(2 > 1);//true
+// console.log(2 >= 1);//true
+// console.log(2 < 1);//false
+// console.log(2 == 1);//false
+// console.log(2 != 1);//true
+
+//make same datatype conversion...so typescript is used
+// console.log("2" > 1);//true  2 converted to no by JS
+// console.log("02" > 1);//true
+
+
+// console.log(null > 0);//false
+// console.log(null == 0);//false
+// console.log(null >= 0);//true
+
+//The reason is: equality check == and comparisons > < >= <= work differently.
+// comparison convert null to a number; treating it as 0..
+// so, (3) null >= 0 is true and (1)null >0 is false..
+
+// console.log(undefined == 0);//false with every conversion too..
+
+/// strict check === check value and datatype
+// console.log("2" === 2);//false[str and int]
+// console.log(2 === 2);//ture
+
+
+
+//undefined and null makes confusion
+
+
+
+
+
+
+
