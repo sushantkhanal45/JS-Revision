394bcddfb2b59432536a9e6d571fdd70af7d74d4
Sushant Khanal <skkhanal45@gmail.com>
2025-11-13 16:08:22 +0545

    conversion concepts

---
+++ str-numConversion.js
@@ -0,0 +1,50 @@
+//operations
+
+//just for concepts; cannot be applied practically....
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue); //-3
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "world"
+let str3 = str1 + str2
+// console.log(str3); //helloworld
+
+// console.log("1" + 2); //12
+// console.log(1 + "2" ); //12
+
+// console.log("1" + 2 + 2 )//122
+
+// console.log(1 + 2 + "2" )//32
+// console.log(1 + "2" + 2 )//122
+// console.log(1 + 2 + "2" )//122
+
+// console.log(true);//true
+// console.log(+true);//1
+// console.log(+"");//0
+
+let count = 100
+// count++;
+// console.log(count);//101
+// ++count;
+// console.log(count);//101
+
+
+
+
+
+
+
+
+
+
+
+
