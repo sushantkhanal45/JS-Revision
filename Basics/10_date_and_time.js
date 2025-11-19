dea20693d5c9537f084ca4be17233bef5997f91f
Sushant Khanal <skkhanal45@gmail.com>
2025-11-19 15:03:09 +0000

    basics cleared

---
+++ Basics/10_date_and_time.js
@@ -0,0 +1,43 @@
+//Dates
+
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString());
+
+// console.log(typeof myDate);
+
+//let myCreatedDate = new Date(2025, 0, 23)// year, month, day//
+// console.log(myCreatedDate.toDateString());
+
+// let myCreatedDate = new Date(2025, 0, 23, 5, 3)
+// console.log(myCreatedDate.toLocaleString());//1/23/2025, 5:03:00 AM
+
+
+let myCreatedDate = new Date("2020-12-10")//yy/mm/dd  use:[mm/dd/yy]
+console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);// ms value from above run date.. 1763564094253..
+
+// console.log(myCreatedDate.getTime());//1607558400000
+
+console.log(Math.floor(Date.now()/1000));// to avoid decimal
+
+let newDate = new Date()
+console.log(newDate.getMonth()+ 1);//mth starts from 0 ..
+console.log(newDate.getDay());
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+   
+})
+
+console.log(newDate);
+
+
+
+
+
