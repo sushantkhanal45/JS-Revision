eeed5eab6db196d9745c36b21e94275995646a2a
Sushant Khanal <skkhanal45@gmail.com>
2026-01-06 18:43:37 +0545

    de-structure and api included

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -47,14 +47,58 @@ const users = [
 ]
 
 users[1].email
-console.log(appUser);
+// console.log(appUser);
 
-console.log(Object.keys(appUser));//[ 'id', 'name', 'isLoggedIn' ]
-console.log(Object.values(appUser));//[ '123abc', 'Sam', false ]
-console.log(Object.entries(appUser));//[ [ 'id', '123abc' ], [ 'name', 'Sam' ], [ 'isLoggedIn', false ] ]... 1st is key and 2 nd is value in array
+// console.log(Object.keys(appUser));//[ 'id', 'name', 'isLoggedIn' ]
+// console.log(Object.values(appUser));//[ '123abc', 'Sam', false ]
+// console.log(Object.entries(appUser));//[ [ 'id', '123abc' ], [ 'name', 'Sam' ], [ 'isLoggedIn', false ] ]... 1st is key and 2 nd is value in array
 
-console.log(appUser.hasOwnProperty('isLogged'));//false
-console.log(appUser.hasOwnProperty('isLoggedIn'));//true
+// console.log(appUser.hasOwnProperty('isLogged'));//false
+// console.log(appUser.hasOwnProperty('isLoggedIn'));//true
+
+
+
+//destructuring & json API
+
+const course ={
+    coursename: "javascript",
+    price: "999",
+    courseIinstructor: "Sushant"
+}
+
+// course.courseIinstructor// not accessing all by course . but with a better technique.
+
+// const {courseIinstructor} = course
+// console.log(courseIinstructor);// as specified..
+
+const {courseIinstructor: instructor} = course
+// console.log(instructor); same as above just specifying instructor in place of courseInstructor..in case of courseinstructor feels ng then can be modified...
+
+
+//the following is of react..
+// const navbar= ({comapny}) => {
+
+
+// }
+
+// navbar(comapny= "abc")// just for concept of desturcturing..
+
+
+//API. previously in XML but now in json
+
+//json//obj in simple..
+// {
+//     name:"Sushant",
+//     coursename: "javascript",
+//     price: "free"
+// }
+//gives error because obj is not defined here just for understanding..
+
+//api can be in obj form or array too..
+
+//randomuser me: famous sie for random api
+
+//json formatter (site) for understanding API
 
 
 
