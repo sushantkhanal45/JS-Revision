19d76e037a58b48d50f7e89d455e15951dd9a9cb
Sushant Khanal <skkhanal45@gmail.com>
2026-01-08 16:29:23 +0545

    scopes and hosting cleared

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -12,7 +12,7 @@ if (true){
 let a = 10
 const b = 20
 var c = 30
-console.log(a);//local(block) scope..
+// console.log(a);//local(block) scope..
 
 }
 
@@ -28,6 +28,57 @@ var c = 300
 // console.log(a);//300 because it is defined in global scope..
 
 
-// if we run from node then global scope is different..for interviews... later on t be studied more..
+// if we run from node then global scope is different..for interviews... later on to be studied more..
 
+
+
+//nested scope..
+
+function one(){
+    const username = "Sushant"
+
+    function two(){
+
+        const website = "youtube"
+        // console.log(username);
+        
+    }
+    // console.log(website);//not accessible..and wont give two() because line line by implementing..it stops..
+    
+    // two()//Sushant
+}
+
+// one() //one() only defines the inner function two().never calls two()
+
+if (true){
+    const username = "Sushant"
+    if(username === "Sushant"){
+        const website = " youtube"
+        // console.log(username + website);
+        
+    }
+    // console.log(website);//not accessible.
+    
+}
+// console.log(username);
+
+// ++++++++++++++++++Interesting++++++++++++++++
+//console.log(addone(5));//can execute..
+function addone(num){
+
+return num+1
+
+}
+
+console.log(addone(5));
+
+//++++++++++++++
+
+//console.log(addTwo(5));//cant access like this because stored by declaration..
+
+const addTwo = function(num){
+    return num + 2
+}
+
+console.log(addTwo(5));
 
