f65d7d2e1d90fe512c41e11155e987647021768b
Sushant Khanal <skkhanal45@gmail.com>
2026-01-30 21:12:16 +0545

    iife revised

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,19 @@
+// Immediately Invoked Function Expression (IIFE)
+
+(function db(){
+    console.log(`DB Connected`);
+    
+}) ();
+
+// () () // frst fun_defn second execution
+//the problem of global scope pollution is handled..
+//semo-colon is necessary in above to mark as it ending...
+( () => {
+    console.log(`DB connected Two`);
+    
+}) ();
+
+( (name) => {
+    console.log(`DB connected To ${name}`);
+    
+}) ('Sushant')
